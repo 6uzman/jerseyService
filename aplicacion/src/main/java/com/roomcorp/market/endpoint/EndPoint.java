5d605fe30a7d6aaf6fa08baaeddd2df569e9a2b9
Jose Guzman <guz.blas@gmail.com>
2015-05-10 15:28:07 -0500

    Initial configuration to jersey services

---
+++ aplicacion/src/main/java/com/roomcorp/market/endpoint/EndPoint.java
@@ -0,0 +1,24 @@
+package com.roomcorp.market.endpoint;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ */
+
+@Component
+@Scope("singleton")
+@Path("/cheer/")
+public class EndPoint {
+  @GET
+  @Path("hello")
+  public Response  sayHello (){
+    System.out.println("MENSAJE: LLegó a endpoint");
+    return Response.status(200).entity("Hello " ).build();
+  }
+}
